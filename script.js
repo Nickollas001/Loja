8aae654ae6b82c372271e2cace3f89823837675a
Nickollas001 <75055375+Nickollas001@users.noreply.github.com>
2023-02-11 09:10:47 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var clickMenu = document.getElementById("click_menu");
+var menu = document.getElementById("menu");
+var fecharMenu = document.getElementById("fecharMenu");
+var header = document.getElementById("header");
+var logo = document.getElementById("logo");
+
+clickMenu.addEventListener("click", function(){
+    menu.style.display = "flex";
+    header.style.backgroundColor ="rgba(0, 0, 0, 0.800)";
+    
+});
+fecharMenu.addEventListener("click",function(){
+    menu.style.display = "none";
+});
+
+logo.addEventListener("click",function(){
+    window.location = "index.html";
+})
