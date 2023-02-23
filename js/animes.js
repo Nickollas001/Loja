bc11687044cb6189c621c203358231cf7da053a5
Nickollas001 <75055375+Nickollas001@users.noreply.github.com>
2023-02-23 00:09:46 -0300

    att

---
+++ js/animes.js
@@ -0,0 +1,55 @@
+
+const containerDaListaDeProdutos = document.querySelector(".listaDeProdutos");
+const campoDePesquisa = document.querySelector("#campoPesquisar");
+
+const tabelaOriginal = [
+     {
+         id: 1,
+         produto: 'Naruto',
+         poster: 'amigurumis/animes/Naruto/Naruto/Naruto-NoBG.png',
+         link: 'animes.html'
+
+     },
+    // {
+    //     id:"2",
+    //     produto: 'Kakashi Hatake',
+    //     poster: 'amigurumis/animes/Naruto/KakashiHatake/kakashiHatake-NoBg.png',
+    //     link: 'amigurumis/animes/Naruto/KakashiHatake/kakashiHatake.html'
+
+    // }
+];
+
+let tabelaDeProdutos = tabelaOriginal;
+
+const carregarListaDeProdutos = function(){
+    containerDaListaDeProdutos.innerHTML = "";
+
+    tabelaDeProdutos.map( function(produto){
+        containerDaListaDeProdutos.innerHTML +=  `
+
+            <div class="produtos">
+            <a href="${produto.link}"><img src="${produto.poster}" alt="${produto.produto}"></a>
+                <h2 class="texto-produtos">${produto.produto}</h2>
+            </div>
+        `
+    })
+};
+
+carregarListaDeProdutos();
+
+function retirar_acentos(text){
+    const a = 'àáäâãèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;';
+    const b = 'aaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+    return text.toString().toLowerCase().trim()
+    .replace(p, c => b.charAt(a.indexOf(c)))
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, ' ');
+};
+
+campoDePesquisa.addEventListener("keyup", function(){
+    const resultado = tabelaDeProdutos.filter((produto)=> retirar_acentos(produto.produto).includes(retirar_acentos(campoDePesquisa.value)));
+    tabelaDeProdutos = resultado;
+    carregarListaDeProdutos();
+    tabelaDeProdutos = tabelaOriginal;
+});
